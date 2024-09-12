76f35b1c3ccac78ec01c7f0415d0c10699ff4b57
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-09-12 13:56:43 +0700

    feat: test swagger :construction:

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,59 @@
 import express, { Express, Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
 import { routerv1 } from './routes/v1';
-
+var cors = require('cors')
+const swaggerUi = require("swagger-ui-express");
+const bodyParser = require("body-parser");
+const swaggerJsdoc = require("swagger-jsdoc");
 //For env File 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+const options = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "LogRocket Express API with Swagger",
+            version: "0.1.0",
+            description:
+            "This is a simple CRUD API application made with Express and documented with Swagger",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+                name: "LogRocket",
+                url: "https://logrocket.com",
+                email: "info@email.com",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+    },
+    apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
+
 app.use("/api/v1", routerv1);
 
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+    console.log(`Server is Fire at http://localhost:${port}`);
 });
+
--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -2,12 +2,14 @@ import express, {Request, Response} from 'express';
 
 export const routerv1 = express.Router();
 
-routerv1.get( '/', (req: Request, res: Response) => {
+routerv1.get( '/', ( _, res) => {
 
-    res.send('Welcome to Express & TypeScript Server');
+    res.json({
+        msg: 'Welcome to Express & TypeScript Server'
+    });
   });
 
-routerv1.get( '/', (req: Request, res: Response) => {
+routerv1.get( '/users', (req: Request, res: Response) => {
 
     res.send('Welcome to Express & TypeScript Server');
   });
